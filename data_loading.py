053f9b31bd635d79e23f10a1e5263fbd5fb9a792
Adam Crocker <77861255+captcrock@users.noreply.github.com>
2023-06-26 12:28:29 -0500

    Update data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -1,12 +1,59 @@
-# Assuming you have a loading function for each dataset.
-def load_airports_data(df):
-    
-    return
+import sqlite3
+from sqlite3 import Error
 
-def load_airlines_data(df):
+def create_connection():
+    """ create a database connection to a SQLite database """
+    conn = None;
+    try:
+        conn = sqlite3.connect(':memory:')       
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
-    return
+def load_airports(airports):
+    """
+    Load the airports data into a SQLite database.
 
-def load_routes_data(df):
-  
-    return
+    Parameters:
+    airports (DataFrame): The airports data.
+    """
+
+    conn = create_connection()
+
+    # Load the data into SQLite
+    airports.to_sql('airports', conn, if_exists='replace', index=False)
+
+    print("Airports data loaded successfully.")
+
+def load_airlines(airlines):
+    """
+    Load the airlines data into a SQLite database.
+
+    Parameters:
+    airlines (DataFrame): The airlines data.
+    """
+
+    conn = create_connection()
+
+    # Load the data into SQLite
+    airlines.to_sql('airlines', conn, if_exists='replace', index=False)
+
+    print("Airlines data loaded successfully.")
+
+def load_routes(routes):
+    """
+    Load the routes data into a SQLite database.
+
+    Parameters:
+    routes (DataFrame): The routes data.
+    """
+
+    conn = create_connection()
+
+    # Load the data into SQLite
+    routes.to_sql('routes', conn, if_exists='replace', index=False)
+
+    print("Routes data loaded successfully.")
