993ff3fd1c0d8d81255b1bcb3dd18c7184db9820
Adam Crocker <77861255+captcrock@users.noreply.github.com>
2023-06-26 12:24:18 -0500

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,28 @@
-def analyze_airports_data(df):
-    
-    return
+def analyze_data(airports, airlines, routes):
+    """
+    Analyze the data and return the results.
 
-def analyze_airlines_data(df):
-    
-    return
+    Parameters:
+    airports (DataFrame): The airports data.
+    airlines (DataFrame): The airlines data.
+    routes (DataFrame): The routes data.
 
-def analyze_routes_data(df):
-    
-    return
+    Returns:
+    dict: A dictionary with the results of the data analysis.
+    """
+
+    results = {}
+
+    # Count the number of unique airlines
+    unique_airlines = airlines['Name'].nunique()
+    results['unique_airlines'] = unique_airlines
+
+    # Count the number of unique airports
+    unique_airports = airports['Name'].nunique()
+    results['unique_airports'] = unique_airports
+
+    # Count the number of unique routes
+    unique_routes = routes[['Source airport', 'Destination airport']].drop_duplicates().shape[0]
+    results['unique_routes'] = unique_routes
+
+    return results
