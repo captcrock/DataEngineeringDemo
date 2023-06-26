e48c082c08168e407a7af4d929c2b9885e6db311
Adam Crocker <77861255+captcrock@users.noreply.github.com>
2023-06-26 11:51:44 -0500

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,45 @@
-def clean_airports_data(df):
-  
-    return df
+import pandas as pd
 
-def clean_airlines_data(df):
-  
-    return df
+def clean_airports_data(airports):
+    """
+    Cleans the airports data.
+    """
+    # Remove any rows with null values
+    airports = airports.dropna()
 
-def clean_routes_data(df):
- 
-    return df
+    # Convert IDs to integers
+    airports['Airport ID'] = airports['Airport ID'].astype(int)
+
+    # Convert Latitude, Longitude and Altitude to floats
+    airports['Latitude'] = airports['Latitude'].astype(float)
+    airports['Longitude'] = airports['Longitude'].astype(float)
+    airports['Altitude'] = airports['Altitude'].astype(float)
+
+    return airports
+
+def clean_airlines_data(airlines):
+    """
+    Cleans the airlines data.
+    """
+    # Remove any rows with null values
+    airlines = airlines.dropna()
+
+    # Convert IDs to integers
+    airlines['Airline ID'] = airlines['Airline ID'].astype(int)
+
+    # Convert 'Active' to boolean
+    airlines['Active'] = airlines['Active'] == 'Y'
+
+    return airlines
+
+def clean_routes_data(routes):
+    """
+    Cleans the routes data.
+    """
+    # Remove any rows with null values
+    routes = routes.dropna()
+
+    # Convert IDs to integers
+    routes['Airline ID'] = routes['Airline ID'].astype(int)
+
+    return routes
