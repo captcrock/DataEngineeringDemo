b5ce509653214f3306b965d331dfc06ac7b143af
Adam Crocker <77861255+captcrock@users.noreply.github.com>
2023-06-26 11:23:22 -0500

    Create data_loading.py

---
+++ data_loading.py
@@ -0,0 +1,12 @@
+# Assuming you have a loading function for each dataset.
+def load_airports_data(df):
+    
+    return
+
+def load_airlines_data(df):
+
+    return
+
+def load_routes_data(df):
+  
+    return
